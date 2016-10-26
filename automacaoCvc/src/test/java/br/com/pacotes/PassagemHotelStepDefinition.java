3fec69f1eb89e498377fbc855f2f865b2b9976c2
Douglas Martins Santos <douglassantos@cvc.com.br>
2016-10-26 18:54:37 -0200

    third

--- automacaoCvc/src/test/java/br/com/pacotes/PassagemHotelStepDefinition.java
+++ automacaoCvc/src/test/java/br/com/pacotes/PassagemHotelStepDefinition.java
@@ -1,5 +1,7 @@
 package br.com.pacotes;
 
+import java.io.IOException;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -11,24 +13,54 @@ import cucumber.api.java.en.When;
 public class PassagemHotelStepDefinition {
 
 	WebDriver driver;
-
+   /*
 	@Given("^Acessar endereco e navegador$")
-	public void acessar_endereco_e_navegador() throws Throwable {
-		System.setProperty("webdriver.gecko.driver", "C:\\geckodriver-v0.10.0-win64\\geckodriver.exe");
+	public void acessar_endereco_e_navegador() throws InterruptedException, IOException {
+		System.setProperty("webdriver.gecko.driver",
+				"C:\\geckodriver-v0.10.0-win64\\geckodriver.exe");
 		driver = new FirefoxDriver();
-		driver.get("http://vendashom.cvc.com.br/index.aspx");
-						
+
+		openHome(driver);
+		masterPrice(driver);
+		checkout(driver);
+
 	}
 
-	@When("^Executar venda de passagem mas hotel$")
-	public void executar_venda_de_passagem_mas_hotel() throws Throwable {
-		driver.findElement(By.id("txtOrigin")).sendKeys("São Paulo");
-		driver.findElement(By.id("txtDestination")).sendKeys("Maceio");
-		driver.findElement(By.id("btnSearch")).click();
+	private void openHome(WebDriver driver2) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	
+	private void masterPrice(WebDriver driver2) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	private void checkout(WebDriver driver2) {
+		// TODO Auto-generated method stub
+		
 	}
 
-	@Then("^Fechar browser$")
-	public void fechar_browser() throws Throwable {
-	    
+	
+
+	private void screenshotErro(WebDriver driver2) {
+		// TODO Auto-generated method stub
+
 	}
+
+	
+
+	
+	
+	  @When("^Executar venda de  passagem mais hotel$") public void
+	  executar_venda_de_passagem_mais_hotel() throws Throwable {
+		  
+	  }
+	  
+	  @Then("^Fechar browser$") public void fechar_browser() throws Throwable {
+	  
+	  }
+*/	 
+
 }
--- automacaoCvc/src/test/resource/PassagemHotel.feature
+++ automacaoCvc/src/test/resource/PassagemHotel.feature
@@ -1,6 +1,28 @@
 Feature: Compra de passagem mas hotel
+  Para quando uma viagem for solicitada
+  Eu, como vendedor
+  Desejo que a venda seja concluida
 
-  Scenario: 
-    Given Acessar endereco e navegador
-    When Executar venda de passagem mas hotel
-    Then Fechar browser
+  Scenario: Prencher tela inicial do site e clicar no botão de confirmação
+    Given Estou no site da cvc
+    When Preencho a localização do Estado que sairei para a viagem
+    And Preencho o local onde passarei a viagem
+    And Preencho a data de saida
+    And Preencho a data de volta
+    And Preencho a quantidade quartos preciso na viagem
+    And Preencho a quanitdade de adultos que irão na viagem
+    And Preencho a quantidade de crianças que irão na viagem
+    And Preesione o botão de buscar demostrando interesse na viagem
+    Then Mostra tela de escolha de hotel e empresa aerea
+
+  Scenario: Escolher hotel e empresa aerea solicitada
+    Given
+    When
+    Then
+
+
+  Scenario: Preencher dados de compra do cliente e de todos os hospedes
+    Given
+    When
+
+    Then Sistema exibe numero de reserva
